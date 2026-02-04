9941cd19b2807d71c19ab0078d7c98889c4c03d2
Preranabarat <preranabarat@2004>
2026-02-04 12:41:25 +0530

    update

---
+++ broCode_javaFullCource/intro.java
@@ -0,0 +1,25 @@
+package broCode_javaFullCource;
+
+import java.util.Scanner;
+
+public class intro {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+
+        String name = scanner.nextLine();
+        System.out.print("Enter your roll number : ");
+
+        int roll = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter your section : ");
+        String sec = scanner.nextLine();
+
+        System.out.println("Hello My name is :" + name);
+        System.out.println("My roll no is :" + roll);
+        System.out.println("My section is : " + sec);
+        scanner.close();
+    
+    }
+    
+}
