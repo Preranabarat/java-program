741f7f75e78ee5e955febdb1d33e6cfd6802326b
Preranabarat <preranabarat@2004>
2026-02-05 13:25:44 +0530

    grammer Game

---
+++ broCode_javaFullCource/madLibsGame.java
@@ -0,0 +1,32 @@
+package broCode_javaFullCource;
+import java.util.Scanner;
+public class madLibsGame {
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         scanner.nextLine();
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+        
+        System.out.print("Enter an adjective(description):");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter a noun(animal or person):");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter an adjective(description):");
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter a verb and with -ing(action):");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter an adjective(description):");
+        adjective3 = scanner.nextLine();
+
+        System.out.println("Today I went to a " + adjective1 +"zoo.");
+        System.out.println("In an exibit, i saw a"+ noun1 +".");
+        System.out.println(noun1 + "was " + adjective2+"and "+verb1+"!");
+        System.out.println("i was " + adjective3+"!");
+
+        scanner.close();
+    }
+    
+}
