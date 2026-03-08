f6d57be71ccbf5972199bd7c47a787ba61554b8b
Preranabarat <preranabarat@2004>
2026-03-08 17:31:27 +0530

    calculator program

--- broCode/calculator.java
+++ broCode/calculator.java
@@ -14,17 +14,34 @@ public class calculator {
         System.out.println("Enter the operator(+, -, *, /, ^) ");
         char operator = sc.next().charAt(0);
 
+        boolean  validOperator = true;
+
         switch(operator){
             case '+' -> result = num1 + num2 ;
             case '-' -> result = num1 - num2 ;
             case '*' -> result = num1 * num2 ;
-            case '/' -> result = num1 / num2 ;
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.println();
+                    validOperator =false;
+                }
+                else{
+                    result =num1 / num2;
+                }
+            }
             case '^' -> result = Math.pow(num1, num2) ;
 
-            default -> System.out.println(operator + " Enter the valid operator !");
-        
+            default -> 
+            {
+                System.out.println( "invalid operator !");
+                validOperator = false;
+            }
         }
-        System.out.println("The result is = " + result);
+        if(validOperator){
+            System.out.println("The result is = " + result);
+
+        }
+        
         
         sc.close();
     }
