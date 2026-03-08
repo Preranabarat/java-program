94e30061c1af3c02759710531daf55d09c5e97b5
Preranabarat <preranabarat@2004>
2026-03-08 17:11:14 +0530

    calculator program

---
+++ broCode/calculator.java
@@ -0,0 +1,31 @@
+package broCode;
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number :");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter the second number :");
+        double num2=sc.nextDouble();
+
+        double result=0;
+
+        System.out.println("Enter the operator(+, -, *, /, ^) ");
+        char operator = sc.next().charAt(0);
+
+        switch(operator){
+            case '+' -> result = num1 + num2 ;
+            case '-' -> result = num1 - num2 ;
+            case '*' -> result = num1 * num2 ;
+            case '/' -> result = num1 / num2 ;
+            case '^' -> result = Math.pow(num1, num2) ;
+
+            default -> System.out.println(operator + " Enter the valid operator !");
+        
+        }
+        System.out.println("The result is = " + result);
+        
+        sc.close();
+    }
+}
